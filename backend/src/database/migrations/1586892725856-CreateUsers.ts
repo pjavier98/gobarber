1e595435987fc9d055d28403216a063660b9f7f3
Pedro Javier Paneca Cordova <pjpc@ic.ufal.br>
2020-04-14 17:19:00 -0300

    Relacionamento nos models

--- backend/src/database/migrations/1586892725856-CreateUsers.ts
+++ backend/src/database/migrations/1586892725856-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1586892725856 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
