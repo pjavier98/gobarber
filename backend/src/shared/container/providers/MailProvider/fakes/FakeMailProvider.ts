41162392c7a95f69f08e846c77b77455037f510e
Pedro Javier <pjpc@ic.ufal.br>
2020-05-12 15:57:20 -0300

    Template de emails

--- backend/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,17 +1,10 @@
 import IMailProvider from '../models/IMailProvider';
-
-interface IMessage {
-  to: string;
-  body: string;
-}
+import ISendEmailDTO from '../dtos/ISendMailDTO';
 
 export default class FakeMailProvider implements IMailProvider {
-  private messages: IMessage[] = [];
+  private messages: ISendEmailDTO[] = [];
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.messages.push({
-      to,
-      body,
-    });
+  public async sendMail(message: ISendEmailDTO): Promise<void> {
+    this.messages.push(message);
   }
 }
