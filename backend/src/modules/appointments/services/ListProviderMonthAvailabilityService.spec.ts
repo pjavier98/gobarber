6bdf5d90763469b9bd1662fd8f4c1adccb2cbd56
Pedro Javier <pjpc@ic.ufal.br>
2020-05-13 16:07:13 -0300

    Listando horários disponíveis

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -14,27 +14,21 @@ describe('ListProviderMonthAvailability', () => {
   });
 
   it('should be able to list the month availability from provider', async () => {
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 3, 20, 8, 0, 0),
-    });
+    const hoursAvailablesInDay = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
+    hoursAvailablesInDay.forEach(async hour =>
+      fakeAppointmentsRepository.create({
+        provider_id: 'user',
+        date: new Date(2020, 4, 20, hour, 0, 0),
+      }),
+    );
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       date: new Date(2020, 4, 21, 8, 0, 0),
     });
 
-    const availability = listProviderMonthAvailability.execute({
+    const availability = await listProviderMonthAvailability.execute({
       provider_id: 'user',
       month: 5,
       year: 2020,
@@ -42,10 +36,10 @@ describe('ListProviderMonthAvailability', () => {
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, availability: true },
-        { day: 20, availability: false },
-        { day: 21, availability: false },
-        { day: 22, availability: true },
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
       ]),
     );
   });
