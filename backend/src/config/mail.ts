d8d2f20173c7aa41437c6a94d4790244fd7d79ef
Pedro Javier <pjpc@ic.ufal.br>
2020-05-15 20:20:33 -0300

    Emails pelo Amazon SES

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'javier@gobarber.live',
+      name: 'Pedro Javier do GoBarber',
+    },
+  },
+} as IMailConfig;
