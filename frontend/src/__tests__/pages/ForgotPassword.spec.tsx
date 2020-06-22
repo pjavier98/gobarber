31f23f53d3173c51b22e144933e37faefe3412c8
Pedro Javier <pjpc@ic.ufal.br>
2020-06-22 14:31:44 -0300

    Pagina ForgotPassword testada

---
+++ frontend/src/__tests__/pages/ForgotPassword.spec.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+
+import ForgotPassword from '../../pages/ForgotPassword';
+import api from '../../services/api';
+
+const mockedAddToast = jest.fn();
+const mockedApi = new MockAdapter(api);
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
+    }),
+  };
+});
+
+describe('ResetPassword Page', () => {
+  beforeEach(() => {
+    mockedAddToast.mockClear();
+  });
+
+  it('should be able to recover the password', async () => {
+    const { getByPlaceholderText, getByText } = render(<ForgotPassword />);
+
+    mockedApi.onPost('/password/forgot').replyOnce(200);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const buttonElement = getByText('Recuperar');
+
+    fireEvent.change(emailField, { target: { value: 'jhondoe@example.com' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'success',
+        }),
+      );
+    });
+  });
+
+  it('should not be able to recover the password with invalid email', async () => {
+    const { getByPlaceholderText, getByText } = render(<ForgotPassword />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const buttonElement = getByText('Recuperar');
+
+    fireEvent.change(emailField, { target: { value: 'invalid-email' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedAddToast).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should not be able to recover the password if has an error', async () => {
+    const { getByPlaceholderText, getByText } = render(<ForgotPassword />);
+
+    mockedApi.onPost('/password/forgot').replyOnce(400);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const buttonElement = getByText('Recuperar');
+
+    fireEvent.change(emailField, { target: { value: 'jhondoe@example.com' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+        }),
+      );
+    });
+  });
+});
